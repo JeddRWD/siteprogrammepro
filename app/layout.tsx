215940d30aa2b0cb90e1da1d076da921edaee992
JeddRWD <jedd@rwderbyshire.co.uk>
2026-05-03 21:19:08 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
             </div>
 
             <nav>
+<a href="/login">Login</a>
               <a href="/dashboard">Dashboard</a>
               <a href="/sites">Sites</a>
               <a href="/programme">Programme</a>
